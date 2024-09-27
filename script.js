b43ac2730db0c4b27e98cc530546b6705db5fe60
apurakshit2001 <okritrimoj@gmail.com>
2024-09-27 15:48:02 +0530

    "Updated CSS to add media query and new styles, modified HTML to add multiple custom divs, and updated JavaScript to target individual divs and apply random border radius."

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const div = document.querySelector('.custom-div');
+const div1 = document.querySelector('.item1');
+const div2 = document.querySelector('.item2');
+const div3 = document.querySelector('.item3');
+const div4 = document.querySelector('.item4');
+
 
 const getRandomValue = () => {
     return (Math.floor(Math.random() * 500));
@@ -10,7 +14,10 @@ const applyRandomBorderRadius = () => {
     const rand3 = getRandomValue();
     const rand4 = getRandomValue();
 
-    div.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
+    div1.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
+    div2.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
+    div3.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
+    div4.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
 }
 
 setInterval(() => {
