b48a5b82191f609b26a8bc1321d56a9689e52e3f
apurakshit2001 <okritrimoj@gmail.com>
2024-09-27 16:07:55 +0530

    "Split single `applyRandomBorderRadius` function into four separate functions, one for each div element, and updated `setInterval` to call each new function."

--- script.js
+++ script.js
@@ -8,18 +8,42 @@ const getRandomValue = () => {
     return (Math.floor(Math.random() * 500));
 }
 
-const applyRandomBorderRadius = () => {
-    const rand1 = getRandomValue();
-    const rand2 = getRandomValue();
-    const rand3 = getRandomValue();
-    const rand4 = getRandomValue();
+const applyRandomBorderRadius1 = () => {
+    let rand1 = getRandomValue();
+    let rand2 = getRandomValue();
+    let rand3 = getRandomValue();
+    let rand4 = getRandomValue();
 
     div1.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
+}
+const applyRandomBorderRadius2 = () => {
+    let rand1 = getRandomValue();
+    let rand2 = getRandomValue();
+    let rand3 = getRandomValue();
+    let rand4 = getRandomValue();
+
     div2.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
+}
+const applyRandomBorderRadius3 = () => {
+    let rand1 = getRandomValue();
+    let rand2 = getRandomValue();
+    let rand3 = getRandomValue();
+    let rand4 = getRandomValue();
+
     div3.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
+}
+const applyRandomBorderRadius4 = () => {
+    let rand1 = getRandomValue();
+    let rand2 = getRandomValue();
+    let rand3 = getRandomValue();
+    let rand4 = getRandomValue();
+
     div4.style.borderRadius = `${rand1}px ${rand2}px ${rand3}px ${rand4}px`;
 }
 
 setInterval(() => {
-    applyRandomBorderRadius()
+    applyRandomBorderRadius1();
+    applyRandomBorderRadius2();
+    applyRandomBorderRadius3();
+    applyRandomBorderRadius4();
 }, 200);
